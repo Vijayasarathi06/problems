0262c4d3fe7c99cb8083ff072364e4046ced5ba3
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-17 12:13:02 +0530

    Task:  problems

---
+++ sarathi.py
@@ -0,0 +1,38 @@
+####Exercise:3
+####Bitwise operator
+##p=5
+##q=8
+##print(p^q)
+##print(~p)
+####Exercise:2
+####boolean and logical operator
+##is_sunny=False
+##is_warm=True
+##is_sunny_and_warm=is_sunny and is_warm or is_sunny
+##print("it is sunny and warm:",is_sunny_and_warm)
+##Exercise:1
+##num=int(input("enter your number:"))
+##num +=3
+##num *=2
+##print("the result is num",num)
+##combining string operator
+##Name=str(input("enter the name"))
+##Age=str(input("enter the age"))
+##print("My name is",Name,"and i am ",Age,"years old.")
+##problem:8
+####Modulo and even-odd check
+##even=int(input("enter the number of even"))
+##odd=int(input("enter the number of odd"))
+##number=even%odd
+##print("indicating whether number is",number)
+##
+
+
+
+
+
+
+
+
+
+
